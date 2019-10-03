1305ed2c7fb0b169f104b488e409a85bedde0386
JAPM <cicelcup@gmail.com>
2019-10-03 10:22:11 -0600

    Setting only one Screen for all the buttons

--- app/src/main/java/com/example/hubffee/MainActivity.java
+++ app/src/main/java/com/example/hubffee/MainActivity.java
@@ -13,21 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openScreen1(View view) {
-    }
-
-    public void openScreen2(View view) {
-    }
-
-    public void openScreen3(View view) {
-    }
-
-    public void openScreen4(View view) {
-    }
-
-    public void openScreen5(View view) {
-    }
-
-    public void openScreen6(View view) {
+    public void openScreen(View view) {
     }
 }
