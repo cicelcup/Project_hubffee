b4553abd6adc8f4331d420dd5f6dbbd920d2a3da
JAPM <cicelcup@gmail.com>
2019-10-04 14:37:08 -0600

    Creating two styles and applying the logic to the code (deleting the background of the buttons)

--- app/src/main/java/com/example/hubffee/MainActivity.java
+++ app/src/main/java/com/example/hubffee/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.hubffee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private  SharedPreferences myPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        myPreference = getPreferences(MODE_PRIVATE);
+        int theme = myPreference.getInt("theme",1);
+
+        if (theme == 1){
+            setTheme(R.style.AppTheme);
+        }
+        else
+        {
+            setTheme(R.style.AppTheme_2);
+        }
+
         setContentView(R.layout.activity_main);
     }
 
     public void openScreen(View view) {
+        SharedPreferences.Editor editor = myPreference.edit();
+        editor.putInt("theme",2);
+        editor.commit();
+        recreate();
     }
 }
