c643e028e914c26c47ef4d3228a445e0e0a93fec
JAPM <cicelcup@gmail.com>
2019-10-04 15:25:22 -0600

    Changes in the Main Activity

--- app/src/main/java/com/example/hubffee/MainActivity.java
+++ app/src/main/java/com/example/hubffee/MainActivity.java
@@ -6,30 +6,61 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+//    Variables to recall the last preference for the theme
     private  SharedPreferences myPreference;
+    private int theme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        myPreference = getPreferences(MODE_PRIVATE);
-        int theme = myPreference.getInt("theme",1);
-
-        if (theme == 1){
-            setTheme(R.style.AppTheme);
-        }
-        else
-        {
-            setTheme(R.style.AppTheme_2);
-        }
+//        Setting the App Theme
+        settingAppTheme();
 
         setContentView(R.layout.activity_main);
     }
 
+//    Function to setting the App Theme
+    private void settingAppTheme() {
+        myPreference = getPreferences(MODE_PRIVATE);
+        theme = myPreference.getInt("theme",1);
+
+        switch (theme){
+            case 1:
+                setTheme(R.style.AppTheme);
+                break;
+            case 2:
+                setTheme(R.style.AppTheme_2);
+                break;
+            case 3:
+                setTheme(R.style.AppTheme_3);
+                break;
+            case 4:
+                setTheme(R.style.AppTheme_4);
+                break;
+            case 5:
+                setTheme(R.style.AppTheme_5);
+                break;
+        }
+    }
+
     public void openScreen(View view) {
+//        changing the app theme
+        changingAppTheme();
+    }
+
+//    function to change the app theme
+    private void changingAppTheme() {
         SharedPreferences.Editor editor = myPreference.edit();
-        editor.putInt("theme",2);
+        if (theme < 5){
+            theme ++;
+        }
+        else{
+            theme = 1;
+        }
+
+        editor.putInt("theme",theme);
         editor.commit();
         recreate();
     }
+
 }
