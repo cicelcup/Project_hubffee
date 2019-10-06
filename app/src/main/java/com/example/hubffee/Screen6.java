1e1b069728043d3dcb863caee02659b6e6eb673a
JAPM <cicelcup@gmail.com>
2019-10-06 17:11:55 -0600

    Adding the preferences to all the screens

--- app/src/main/java/com/example/hubffee/Screen6.java
+++ app/src/main/java/com/example/hubffee/Screen6.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Screen6 extends AppCompatActivity {
+    private Settings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //        Setting the App Theme
+        settings = new Settings(this);
+        //        setting the theme
+        settings.settingAppTheme();
+        //      setting the layout
         setContentView(R.layout.activity_screen6);
     }
 }
