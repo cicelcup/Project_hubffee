962a76e709caed2f9c68431ef5bf9288334435be
JAPM <cicelcup@gmail.com>
2019-10-06 23:36:22 -0600

    Back arrow changed to different colors

--- app/src/main/java/com/example/hubffee/Screen3.java
+++ app/src/main/java/com/example/hubffee/Screen3.java
@@ -2,9 +2,13 @@ package com.example.hubffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Screen3 extends AppCompatActivity {
     private Settings settings;
@@ -23,6 +27,27 @@ public class Screen3 extends AppCompatActivity {
         //      setting the layout
         setContentView(R.layout.activity_screen3);
         configuringScreen();
+    }
+
+    //creating the option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Menu main is a layout created
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    //open the menu selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //Check if setting was pressed
+        if (id == R.id.actions_settings) {
+            Toast.makeText(this,"hello",Toast.LENGTH_SHORT);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void configuringScreen() {
